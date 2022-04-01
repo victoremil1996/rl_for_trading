d44fed524cfc9a127caa21798a304442d35d1cd7
victoremil1996 <Victoremilskovlundmark@gmail.com>
2022-04-01 04:33:33 +0200

    fixed bugs, simulation work, total pnl for all agents is 0, when trading fee is 0.

--- market_simulation_study/environment.py
+++ market_simulation_study/environment.py
@@ -3,25 +3,30 @@ import pandas as pd
 import random
 
 class market_env():
-    def __init__(self, market_prices, fee):
-        self.market_prices = market_prices
-        self.fee = fee
-        
+    def __init__(self,
+                 state: dict):
+        self.state = state
+        self.market_prices = state["market_prices"]
+        self.fee = state["fee"]
+
     def get_latencies(self):
-        latencies = [0] * len(self.agent) 
-        for i in range(len(self.agent)):
-            latencies[i] = self.agent.latency
-        #que_indices = range(len(self.agent))
+        latencies = [0] * len(self.agents) 
+        for i in range(len(self.agents)):
+            latencies[i] = self.agents[i].latency
+        #que_indices = range(len(self.agents))
         
         return latencies
     
     def match(self):
         latencies = self.get_latencies()
         
-        matched_buy = [0] * len(self.agent)
-        matched_sell = [0] * len(self.agent)
+        matched_buy = [0] * len(self.agents)
+        matched_sell = [0] * len(self.agents)
+        matched_buy_price = [0] * len(self.agents)
+        matched_sell_price = [0] * len(self.agents)
         mean_price = 0
-        buyers, sellers = np.array(self.agent).copy(), np.array(self.agent).copy()
+        n_trades = 0
+        buyers, sellers = np.array(self.agents).copy(), np.array(self.agents).copy()
         buyers = [x for _, x in sorted(zip(latencies, buyers))] # Sorting buyers according to latency
         sellers = [x for _, x in sorted(zip(latencies, sellers))] # Sorting sellers according to latency
         
@@ -31,38 +36,36 @@ class market_env():
                     if buyer.buy_price >= seller.sell_price:
                         matched_buy[buyer.agent_id] = 1
                         matched_sell[seller.agent_id] = 1
-                        
-                        print("ORDER FILLED")
-                        mean_price += (buyer.buy_price + seller.sell_price) / 2
-                        
-                        sellers = np.delete(sellers, 1)
+
+                        #print("ORDER FILLED")
+                        trade_price = (buyer.buy_price + seller.sell_price) / 2
+                        matched_buy_price[buyer.agent_id] = trade_price
+                        matched_sell_price[seller.agent_id] = trade_price
+
+                        mean_price += trade_price
+                        n_trades += 1
+                        sellers = np.delete(sellers, j)
                         break
-                    else:
-                        print("ORDER NOT FILLED")
+                    #else:
+                        #print("ORDER NOT FILLED")
         
-        if len(matched_sell) > 0:
-            mean_price /= (len(matched_sell))
+        if np.sum(matched_sell) > 0:
+            mean_price /= n_trades
         else:
             mean_price = self.market_prices[-1]
-            
-        print(mean_price)
+
         self.market_prices.append(mean_price)
-        return matched_buy, matched_sell
+        return matched_buy, matched_sell, matched_buy_price, matched_sell_price
     
     def update_market(self):
         
         return
     
-    def step(self, agent):
-        self.agent = agent
-        matched_buy, matched_sell = self.match()
+    def step(self, agents):
+        self.agents = agents
+        matched_buy, matched_sell, matched_buy_price, matched_sell_price = self.match()
         self.update_market()
-        state = [{'execution_status': np.array([matched_buy, matched_sell]),
-                  'mid_prices': self.market_prices}]
+        state = {'execution_status': np.array([matched_buy, matched_sell, matched_buy_price, matched_sell_price]),
+                 'market_prices': self.market_prices,
+                 'fee': self.fee}
         return state
-    
-    
-    
-    
-    
-    
