e69bfa50ee0aa20f2d41d89ac1c3506d4e4e21da
victoremil1996 <Victoremilskovlundmark@gmail.com>
2022-05-05 11:21:44 +0200

    fan chart

--- codelib/plotting.py
+++ codelib/plotting.py
@@ -190,6 +190,113 @@ def dist_vs_normal_plot(returns, **kwargs):
             transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.5))
 
 
+def fan_chart(x: ndarray, y: ndarray, **kwargs):
+
+    """
+    From Python for the financial economist at CBS. https://github.com/staxmetrics/python_for_the_financial_economist
+    Plots a fan chart.
+
+    If the number of rows of `y` is divisible by 2, the middle row of `y` is plotted as a line in the middle
+
+    Parameters
+    ----------
+    x: ndarray
+        Vector representing the "x-values" of the plot
+    y: ndarray
+        Matrix of data to plot. Number of columns equal to the length of `x`. Number of rows / 2 is equal to the number
+        different colored areas in the plot. It is assumed that values in the first row is smaller than the values in the
+        second row and so on.
+    **kwargs
+        Other keyword-only arguments
+
+    Returns
+    -------
+        None
+
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+            import numpy as np
+            from corelib.plotting import fan_chart
+            data = np.array([np.random.normal(size=1000) * s for s in np.arange(0, 1, 0.1)])
+            percentiles = np.percentile(data, [10, 20, 50, 80, 90], axis=1)
+            fan_chart(np.arange(1, 11, 1), percentiles, labels=['80% CI', '60% CI', 'median'])
+            plt.show()
+
+    """
+
+    # defaults
+    color_perc = "blue"
+    color_median = "red"
+    xlabel = None
+    ylabel = None
+    title = None
+    labels = None
+    initialize_fig = True
+
+    if 'color' in kwargs:
+        color_perc = kwargs['color']
+    if 'color_median' in kwargs:
+        color_median = kwargs['color_median']
+    if 'xlabel' in kwargs:
+        xlabel = kwargs['xlabel']
+    if 'ylabel' in kwargs:
+        ylabel = kwargs['ylabel']
+    if 'title' in kwargs:
+        title = kwargs['title']
+    if 'labels' in kwargs:
+        labels = True
+        labels_to_plot = kwargs['labels']
+    if "fig" in kwargs:
+        fig = kwargs["fig"]
+    if "ax" in kwargs:
+        ax = kwargs["ax"]
+        initialize_fig = False
+
+    number_of_rows = y.shape[0]
+    number_to_plot = number_of_rows // 2
+
+    if labels is None:
+        labels_to_plot = ["" for i in range(number_to_plot + number_of_rows % 2)]
+
+    if initialize_fig:
+        fig, ax = plt.subplots()
+
+    for i in range(number_to_plot):
+
+        # for plotting below
+        values1 = y[i, :]
+        values2 = y[i + 1, :]
+
+        # for plotting above
+        values3 = y[-2 - i, :]
+        values4 = y[-1 - i, :]
+
+        # calculate alpha
+        alpha = 0.95 * (i + 1) / number_to_plot
+
+        ax.fill_between(x, values1, values2, alpha=alpha, color=color_perc, label=labels_to_plot[i])
+        ax.fill_between(x, values3, values4, alpha=alpha, color=color_perc)
+
+    # plot center value with specific color
+    if number_of_rows % 2 == 1:
+        ax.plot(x, y[number_to_plot], color=color_median, label=labels_to_plot[-1])
+
+    # add title
+    plt.title(title)
+    # add label to x axis
+    plt.xlabel(xlabel)
+    # add label to y axis
+    plt.ylabel(ylabel)
+    # legend
+    if labels:
+        ax.legend()
+
+
+
+
 color_map = plt.cm.get_cmap('tab20c')
 
 default_colors = dict()
